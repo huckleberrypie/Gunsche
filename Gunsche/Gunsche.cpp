c1f34adfb2b81066e41f8313d331450832d4b40b
huckleberrypie <peanuthead_069@yahoo.com>
2014-06-26 10:43:23 +0800

    Initial commit
    Hitler is informed he's on Github.

---
+++ Gunsche/Gunsche.cpp
@@ -0,0 +1,33 @@
+// Gunsche.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <cstdlib>
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Fegelein has just written this C++ program. \n";
+	std::system("pause > nul"); 
+	std::cout << "It seems he trapped us in text mode. \n";
+	std::system("pause > nul"); 
+	std::cout << "Of all the antics Fegelein pulls off at me... \n";
+	std::system("pause > nul"); 
+	std::cout << "'Tis infuriating! \n";
+	std::system("pause > nul"); 
+	std::cout << "If he doesn't get us out of this mode, he's fucked! \n";
+	std::system("pause > nul"); 
+	std::cout << "The next time I see that imbecile I'm going to plant a virus up his ass! \n";
+	std::system("pause > nul"); 
+	std::cout << "Preposterous! \n";
+	std::system("pause > nul"); 
+	std::cout << "BRING ME THAT IMBECILE FEGELEIN! \n";
+	std::system("pause > nul"); 
+	std::cout << "FEGELEIN! \n";
+	std::system("pause > nul"); 
+	std::cout << "FEGELEIN!! \n";
+	std::system("pause > nul"); 
+	std::cout << "FEGELEIN!!! \n";
+	std::system("pause > nul"); 
+	return 0;
+}
