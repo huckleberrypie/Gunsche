e8a64acc29d6c7aba9f6429485a3e5901b4e3159
huckleberrypie <peanuthead_069@yahoo.com>
2014-12-18 09:02:50 +0800

    Pause fix
    Because std::system("pause > nul"); is too Windows-specific. :p

--- Gunsche/Gunsche.cpp
+++ Gunsche/Gunsche.cpp
@@ -8,26 +8,26 @@
 int main(int argc, char *argv[])
 {
 	std::cout << "Fegelein has just written this C++ program. \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "It seems he trapped us in text mode. \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "Of all the antics Fegelein pulls off at me... \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "'Tis infuriating! \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "If he doesn't get us out of this mode, he's fucked! \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "The next time I see that imbecile I'm going to plant a virus up his ass! \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "Preposterous! \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "BRING ME THAT IMBECILE FEGELEIN! \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "FEGELEIN! \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "FEGELEIN!! \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	std::cout << "FEGELEIN!!! \n";
-	std::system("pause > nul"); 
+	std::cin.ignore(); 
 	return 0;
 }
